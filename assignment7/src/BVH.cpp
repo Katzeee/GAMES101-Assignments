3fdc8ef3c7ace6794898c2da24600c5d0dbc6c5c
xac <jiangfengxi.c@gmail.com>
2023-04-03 14:12:45 +0800

    path tracing test

--- assignment7/src/BVH.cpp
+++ assignment7/src/BVH.cpp
@@ -1,129 +1,133 @@
 #include <algorithm>
 #include <cassert>
+#include <queue>
 #include "BVH.hpp"
 
-BVHAccel::BVHAccel(std::vector<Object*> p, int maxPrimsInNode,
-                   SplitMethod splitMethod)
-    : maxPrimsInNode(std::min(255, maxPrimsInNode)), splitMethod(splitMethod),
-      primitives(std::move(p))
-{
-    time_t start, stop;
-    time(&start);
-    if (primitives.empty())
-        return;
+BVHAccel::BVHAccel(std::vector<Object *> p, int maxPrimsInNode, SplitMethod splitMethod)
+    : maxPrimsInNode(std::min(255, maxPrimsInNode)), splitMethod(splitMethod), primitives(std::move(p)) {
+  time_t start, stop;
+  time(&start);
+  if (primitives.empty()) return;
 
-    root = recursiveBuild(primitives);
+  root = recursiveBuild(primitives);
 
-    time(&stop);
-    double diff = difftime(stop, start);
-    int hrs = (int)diff / 3600;
-    int mins = ((int)diff / 60) - (hrs * 60);
-    int secs = (int)diff - (hrs * 3600) - (mins * 60);
+  time(&stop);
+  double diff = difftime(stop, start);
+  int hrs = (int)diff / 3600;
+  int mins = ((int)diff / 60) - (hrs * 60);
+  int secs = (int)diff - (hrs * 3600) - (mins * 60);
 
-    printf(
-        "\rBVH Generation complete: \nTime Taken: %i hrs, %i mins, %i secs\n\n",
-        hrs, mins, secs);
+  printf("\rBVH Generation complete: \nTime Taken: %i hrs, %i mins, %i secs\n\n", hrs, mins, secs);
 }
 
-BVHBuildNode* BVHAccel::recursiveBuild(std::vector<Object*> objects)
-{
-    BVHBuildNode* node = new BVHBuildNode();
+BVHBuildNode *BVHAccel::recursiveBuild(std::vector<Object *> objects) {
+  BVHBuildNode *node = new BVHBuildNode();
 
-    // Compute bounds of all primitives in BVH node
-    Bounds3 bounds;
-    for (int i = 0; i < objects.size(); ++i)
-        bounds = Union(bounds, objects[i]->getBounds());
-    if (objects.size() == 1) {
-        // Create leaf _BVHBuildNode_
-        node->bounds = objects[0]->getBounds();
-        node->object = objects[0];
-        node->left = nullptr;
-        node->right = nullptr;
-        node->area = objects[0]->getArea();
-        return node;
-    }
-    else if (objects.size() == 2) {
-        node->left = recursiveBuild(std::vector{objects[0]});
-        node->right = recursiveBuild(std::vector{objects[1]});
-
-        node->bounds = Union(node->left->bounds, node->right->bounds);
-        node->area = node->left->area + node->right->area;
-        return node;
-    }
-    else {
-        Bounds3 centroidBounds;
-        for (int i = 0; i < objects.size(); ++i)
-            centroidBounds =
-                Union(centroidBounds, objects[i]->getBounds().Centroid());
-        int dim = centroidBounds.maxExtent();
-        switch (dim) {
-        case 0:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().x <
-                       f2->getBounds().Centroid().x;
-            });
-            break;
-        case 1:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().y <
-                       f2->getBounds().Centroid().y;
-            });
-            break;
-        case 2:
-            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
-                return f1->getBounds().Centroid().z <
-                       f2->getBounds().Centroid().z;
-            });
-            break;
-        }
-
-        auto beginning = objects.begin();
-        auto middling = objects.begin() + (objects.size() / 2);
-        auto ending = objects.end();
-
-        auto leftshapes = std::vector<Object*>(beginning, middling);
-        auto rightshapes = std::vector<Object*>(middling, ending);
-
-        assert(objects.size() == (leftshapes.size() + rightshapes.size()));
-
-        node->left = recursiveBuild(leftshapes);
-        node->right = recursiveBuild(rightshapes);
-
-        node->bounds = Union(node->left->bounds, node->right->bounds);
-        node->area = node->left->area + node->right->area;
-    }
-
+  // Compute bounds of all primitives in BVH node
+  Bounds3 bounds;
+  for (int i = 0; i < objects.size(); ++i) bounds = Union(bounds, objects[i]->getBounds());
+  if (objects.size() == 1) {
+    // Create leaf _BVHBuildNode_
+    node->bounds = objects[0]->getBounds();
+    node->object = objects[0];
+    node->left = nullptr;
+    node->right = nullptr;
+    node->area = objects[0]->getArea();
     return node;
-}
+  } else if (objects.size() == 2) {
+    node->left = recursiveBuild(std::vector{objects[0]});
+    node->right = recursiveBuild(std::vector{objects[1]});
 
-Intersection BVHAccel::Intersect(const Ray& ray) const
-{
-    Intersection isect;
-    if (!root)
-        return isect;
-    isect = BVHAccel::getIntersection(root, ray);
-    return isect;
-}
-
-Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const
-{
-    // TODO Traverse the BVH to find intersection
-
-}
-
-
-void BVHAccel::getSample(BVHBuildNode* node, float p, Intersection &pos, float &pdf){
-    if(node->left == nullptr || node->right == nullptr){
-        node->object->Sample(pos, pdf);
-        pdf *= node->area;
-        return;
+    node->bounds = Union(node->left->bounds, node->right->bounds);
+    node->area = node->left->area + node->right->area;
+    return node;
+  } else {
+    Bounds3 centroidBounds;
+    for (int i = 0; i < objects.size(); ++i) centroidBounds = Union(centroidBounds, objects[i]->getBounds().Centroid());
+    int dim = centroidBounds.maxExtent();
+    switch (dim) {
+      case 0:
+        std::sort(objects.begin(), objects.end(),
+                  [](auto f1, auto f2) { return f1->getBounds().Centroid().x < f2->getBounds().Centroid().x; });
+        break;
+      case 1:
+        std::sort(objects.begin(), objects.end(),
+                  [](auto f1, auto f2) { return f1->getBounds().Centroid().y < f2->getBounds().Centroid().y; });
+        break;
+      case 2:
+        std::sort(objects.begin(), objects.end(),
+                  [](auto f1, auto f2) { return f1->getBounds().Centroid().z < f2->getBounds().Centroid().z; });
+        break;
     }
-    if(p < node->left->area) getSample(node->left, p, pos, pdf);
-    else getSample(node->right, p - node->left->area, pos, pdf);
+
+    auto beginning = objects.begin();
+    auto middling = objects.begin() + (objects.size() / 2);
+    auto ending = objects.end();
+
+    auto leftshapes = std::vector<Object *>(beginning, middling);
+    auto rightshapes = std::vector<Object *>(middling, ending);
+
+    assert(objects.size() == (leftshapes.size() + rightshapes.size()));
+
+    node->left = recursiveBuild(leftshapes);
+    node->right = recursiveBuild(rightshapes);
+
+    node->bounds = Union(node->left->bounds, node->right->bounds);
+    node->area = node->left->area + node->right->area;
+  }
+
+  return node;
 }
 
-void BVHAccel::Sample(Intersection &pos, float &pdf){
-    float p = std::sqrt(get_random_float()) * root->area;
-    getSample(root, p, pos, pdf);
-    pdf /= root->area;
+Intersection BVHAccel::Intersect(const Ray &ray) const {
+  Intersection isect;
+  if (!root) return isect;
+  isect = BVHAccel::getIntersection(root, ray);
+  return isect;
+}
+
+Intersection BVHAccel::getIntersection(BVHBuildNode *node, const Ray &ray) const {
+  // TODO: Traverse the BVH to find intersection
+  Intersection isect;
+  std::queue<BVHBuildNode *> q;
+  q.push(node);
+  BVHBuildNode *tmp_node;
+  while (!q.empty()) {
+    tmp_node = q.front();
+    q.pop();
+    if (!tmp_node->bounds.IntersectP(ray, ray.direction_inv, {false, false, false})) {
+      continue;
+    }
+    if (tmp_node->left != nullptr) {
+      q.push(tmp_node->left);
+    }
+    if (tmp_node->right != nullptr) {
+      q.push(tmp_node->right);
+    }
+    if (tmp_node->object != nullptr) {
+      auto tmp_isect = tmp_node->object->getIntersection(ray);
+      if (tmp_isect.distance < isect.distance) {
+        isect = tmp_isect;
+      }
+    }
+  }
+  return isect;
+}
+
+void BVHAccel::getSample(BVHBuildNode *node, float p, Intersection &pos, float &pdf) {
+  if (node->left == nullptr || node->right == nullptr) {
+    node->object->Sample(pos, pdf);
+    pdf *= node->area;
+    return;
+  }
+  if (p < node->left->area)
+    getSample(node->left, p, pos, pdf);
+  else
+    getSample(node->right, p - node->left->area, pos, pdf);
+}
+
+void BVHAccel::Sample(Intersection &pos, float &pdf) {
+  float p = std::sqrt(get_random_float()) * root->area;
+  getSample(root, p, pos, pdf);
+  pdf /= root->area;
 }
--- assignment7/src/Scene.cpp
+++ assignment7/src/Scene.cpp
@@ -4,61 +4,74 @@
 
 #include "Scene.hpp"
 
-
 void Scene::buildBVH() {
-    printf(" - Generating BVH...\n\n");
-    this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
+  printf(" - Generating BVH...\n\n");
+  this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
 }
 
-Intersection Scene::intersect(const Ray &ray) const
-{
-    return this->bvh->Intersect(ray);
+Intersection Scene::intersect(const Ray &ray) const { return this->bvh->Intersect(ray); }
+
+void Scene::sampleLight(Intersection &pos, float &pdf) const {
+  float emit_area_sum = 0;
+  for (uint32_t k = 0; k < objects.size(); ++k) {
+    if (objects[k]->hasEmit()) {
+      emit_area_sum += objects[k]->getArea();
+    }
+  }
+  float p = get_random_float() * emit_area_sum;
+  emit_area_sum = 0;
+  for (uint32_t k = 0; k < objects.size(); ++k) {
+    if (objects[k]->hasEmit()) {
+      emit_area_sum += objects[k]->getArea();
+      if (p <= emit_area_sum) {
+        objects[k]->Sample(pos, pdf);
+        break;
+      }
+    }
+  }
 }
 
-void Scene::sampleLight(Intersection &pos, float &pdf) const
-{
-    float emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
-            emit_area_sum += objects[k]->getArea();
-        }
+bool Scene::trace(const Ray &ray, const std::vector<Object *> &objects, float &tNear, uint32_t &index,
+                  Object **hitObject) {
+  *hitObject = nullptr;
+  for (uint32_t k = 0; k < objects.size(); ++k) {
+    float tNearK = kInfinity;
+    uint32_t indexK;
+    Vector2f uvK;
+    if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
+      *hitObject = objects[k];
+      tNear = tNearK;
+      index = indexK;
     }
-    float p = get_random_float() * emit_area_sum;
-    emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
-            emit_area_sum += objects[k]->getArea();
-            if (p <= emit_area_sum){
-                objects[k]->Sample(pos, pdf);
-                break;
-            }
-        }
-    }
-}
+  }
 
-bool Scene::trace(
-        const Ray &ray,
-        const std::vector<Object*> &objects,
-        float &tNear, uint32_t &index, Object **hitObject)
-{
-    *hitObject = nullptr;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        float tNearK = kInfinity;
-        uint32_t indexK;
-        Vector2f uvK;
-        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
-            *hitObject = objects[k];
-            tNear = tNearK;
-            index = indexK;
-        }
-    }
-
-
-    return (*hitObject != nullptr);
+  return (*hitObject != nullptr);
 }
 
 // Implementation of Path Tracing
-Vector3f Scene::castRay(const Ray &ray, int depth) const
-{
-    // TO DO Implement Path Tracing Algorithm here
+Vector3f Scene::castRay(const Ray &ray, int depth) const {
+  Vector3f color{0, 0, 0};
+  if (depth > maxDepth) {
+    return color;
+  }
+  // Find out the out light intersect with which object
+  auto p_insect = intersect(ray);
+  if (!p_insect.happened) {
+    return color;
+  }
+  // Find out the direct light contribution
+  Intersection light_sample_insect;
+  float light_pdf;
+  sampleLight(light_sample_insect, light_pdf);
+  auto wi_dir = light_sample_insect.coords - p_insect.coords;
+  auto N = p_insect.normal;
+  Vector3f wi_orig = (dotProduct(wi_dir, N) < 0) ? p_insect.coords - N * EPSILON : p_insect.coords + N * EPSILON;
+  Ray wi{wi_orig, wi_dir.normalized()};
+  auto wi_insect = intersect(wi);
+  if ((wi(wi_insect.distance) - light_sample_insect.coords).norm() < 0.1) {
+    float cost = dotProduct(p_insect.normal, wi.direction);
+    color += p_insect.m->eval(wi.direction, ray.direction, p_insect.normal) / light_pdf;
+  }
+
+  return {};
 }
